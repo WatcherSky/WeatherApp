df54458bbf2e3e727cc9d8f8d0ba5c3e33771ae0
WatcherSky <theworld.com@mail.ru>
2021-07-27 20:46:33 +0600

    commit hometask 26

--- Hometask 25/Hometask 25/ViewController.swift
+++ Hometask 25/Hometask 25/ViewController.swift
@@ -6,9 +6,14 @@
 //
 import Foundation
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     private var apikey = "43de0ac13c66a891d31d59320d9bb346"
+    private var nameCity = "Moscow"
+    private var latitude: CLLocationDegrees?
+    private var longitude: CLLocationDegrees?
+    private var locationManager: CLLocationManager?
     private var temp: Double = 0
     private var descriptionOfWeather: String = ""
     private var feelsLike: Double = 0
@@ -16,25 +21,30 @@ class ViewController: UIViewController {
     private var speed: Double = 0
     private var id: Int = 0
     private var weather: [Weather]?
-    private var nameCity = "Moscow"
+    
     
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-    
     @IBOutlet weak var feelsLikeLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var windLabel: UILabel!
-    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getWeatherData(city: nameCity)
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest;
+        latitude = locationManager?.location?.coordinate.latitude
+        longitude = locationManager?.location?.coordinate.longitude
+        
+        getWeatherDataByCity(city: nameCity)
     }
     
-    
-    private func getWeatherData(city: String) {
+    private func getWeatherDataByCity(city: String) {
         let request = URLRequest(url: URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apikey)")!)
         nameCity = city
         let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
@@ -47,6 +57,38 @@ class ViewController: UIViewController {
                 self.temp = round(weatherDataFromJSON.main.temp - 273)
                 self.descriptionOfWeather = weather.description
                 self.feelsLike = round(weatherDataFromJSON.main.feelsLike - 273)
+                self.humidity = weatherDataFromJSON.main.humidity
+                self.speed = weatherDataFromJSON.wind.speed
+                self.id = weather.id
+                DispatchQueue.main.async {
+                    self.cityLabel.text = "Weather of your City"
+                    self.temperatureLabel.text = "Temperature: \(self.temp) Cº"
+                    self.descriptionLabel.text = "Description: \(self.descriptionOfWeather)"
+                    self.feelsLikeLabel.text = "FeelsLike: \(self.feelsLike) Cº"
+                    self.humidityLabel.text = "Humidity: \(self.id)%"
+                    self.windLabel.text = "Wind: \(self.speed) km/h"
+                    self.imageView.image = UIImage(systemName: self.getIconImage(idIcon: self.id))
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func getWeatherDataByCoordinates() {
+        guard let latitude = latitude, let longitude = longitude else {
+            return
+        }
+        let request = URLRequest(url: URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=43de0ac13c66a891d31d59320d9bb346&units=metric")!)
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            let weatherDataFromJSON = try? JSONDecoder().decode(WeatherData.self, from: data!)
+            
+            guard let weather = weatherDataFromJSON?.weather.first else {
+                return
+            }
+            if let weatherDataFromJSON = weatherDataFromJSON {
+                self.temp = round(weatherDataFromJSON.main.temp)
+                self.descriptionOfWeather = weather.description
+                self.feelsLike = round(weatherDataFromJSON.main.feelsLike)
                 self.humidity = weatherDataFromJSON.main.humidity
                 self.speed = weatherDataFromJSON.wind.speed
                 self.id = weather.id
@@ -96,7 +138,7 @@ class ViewController: UIViewController {
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
                 if let _ = textField.text {
                     cityToSearth = textField.text!
-                    self.getWeatherData(city: cityToSearth)
+                    self.getWeatherDataByCity(city: cityToSearth)
                 }
             }))
         }
@@ -104,11 +146,21 @@ class ViewController: UIViewController {
         return cityToSearth
     }
     
+    @IBAction func locationButton(_ sender: UIButton) {
+        getWeatherDataByCoordinates()
+    }
     
     @IBAction private func searchButton(_ sender: UIButton) {
         displaySearchAlert()
     }
-    
 }
-
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
+}
 
