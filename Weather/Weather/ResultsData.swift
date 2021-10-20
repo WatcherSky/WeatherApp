3dbe5d072cc442183cd16f1df5b61d05f091e128
WatcherSky <theworld.com@mail.ru>
2021-10-20 23:21:51 +0600

    commit startFile Weather

---
+++ Weather/Weather/ResultsData.swift
@@ -0,0 +1,42 @@
+//
+//  ResultsData.swift
+//  Evaluation Test App
+//
+//  Created by Владимир on 02.10.2021.
+//
+
+import Foundation
+
+struct Main: Codable {
+    enum CodingKeys: String, CodingKey {
+        case temp = "temp"
+        case feelsLike = "feels_like"
+        case humidity = "humidity"
+    }
+    let temp: Double
+    let feelsLike: Double
+    let humidity: Int
+}
+
+struct Wind: Codable {
+    enum CodingKeys: String, CodingKey {
+        case speed = "speed"
+    }
+    let speed: Double
+}
+
+struct Weather: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case description = "description"
+    }
+    let id: Int
+    let description: String
+}
+
+struct WeatherData: Codable {
+    let main: Main
+    let wind: Wind
+    let weather: [Weather]
+}
+
